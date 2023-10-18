8e1def541122e003386b7124848ed70133f040ca
jmattaa <mattajonathan1@gmail.com>
2023-10-18 20:00:55 +0200

    Animating about image now too
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,20 +5,22 @@ const AboutMe = () => {
     return (
         <div className="h-1/2 w-screen flex flex-col items-center">
             <div className="grid grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-4 p-8">
-                <div className="order-last lg:order-first"> 
-                    <AnimatedText 
+                <div className="order-last lg:order-first">
+                    <AnimatedText
                         className="text-black text-4xl mb-2 lg:text-white"
                         text="About me"
                         delay={1}
                         y={20}
                     />
-                    <p className="">Hello I'm Jonathan</p>
                 </div>
 
-                <img
-                    className="h-auto object-scale-down order-first  lg:order-last"
+                <motion.img
+                    className="h-auto object-scale-down order-first lg:order-last"
                     src="/img/aboutme.png"
                     alt="me"
+                    initial={{ opacity: 0, translateY: 30 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ delay: 2, duration: .4 }}
                 />
             </div>
         </div>
