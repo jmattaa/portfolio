211e0ae1223631493df2f82cec4e955e91942dcc
jmattaa <mattajonathan1@gmail.com>
2023-11-18 17:55:46 +0100

    Initial commit
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "*.html",
+        "**.html"
+    ],
+    theme: {
+        colors: {
+            "palette-1": "#40bbc2",
+            "palette-2": "#33969c",
+            "palette-3": "#277075",
+            "palette-4": "#1b4b4e",
+            "palette-5": "#0e2728",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
