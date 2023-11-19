438edcc96924e57ddbc6d29116e77ef32af84fcf
jmattaa <mattajonathan1@gmail.com>
2023-11-19 19:55:52 +0100

    I'm a genius Accessing tailwind config in typescript i AM a GENIUS
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

---
+++ src/tailwind-config.ts
@@ -0,0 +1,20 @@
+const conf = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            "palette-1": "#d8f3dd",
+            "palette-2": "#b2e6ba",
+            "palette-3": "#8dd899",
+            "palette-4": "#68ca77",
+            "palette-5": "#43bc56",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
+export default conf
+
