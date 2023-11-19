b243e74535ba35234d3f03c2f9c0758179c6c807
jmattaa <mattajonathan1@gmail.com>
2023-11-19 18:45:52 +0100

    Migrate to React/Typescript-SWC
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "*.html",
-        "**.html",
-        "src/**"
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         colors: {
@@ -11,8 +10,7 @@ export default {
             "palette-2": "#b2e6ba",
             "palette-3": "#8dd899",
             "palette-4": "#68ca77",
-            "palette-5": "#43bc56"
-,
+            "palette-5": "#43bc56",
         },
         extend: {},
     },
