f08ea8062482440bde1c4a2095c0039708439e35
jmattaa <mattajonathan1@gmail.com>
2023-11-28 20:08:17 +0100

    Add about
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
     return (
         <>
             <Nav />
             <Hero />
+            <About />
         </>
     )
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+
+function About() {
+    return (
+        <div className='w-full flex flex-col'>
+            <h1 className='text-3xl text-center text-palette-3 
+                            lg:text-5xl xl:text-6xl'
+            >
+                Who am I?
+            </h1>
+        </div>
+    )
+}
+
+export default About; 
