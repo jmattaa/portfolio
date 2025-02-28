ca6718c6b592e6f9a4dcd0c99ff47d2b7bc40131
jmattaa <mattajonathan1@gmail.com>
2025-02-28 16:08:41 +0100

    refactor html templates, and make it a bit easier on the eye

--- admin/home.go
+++ admin/home.go
@@ -3,12 +3,6 @@ package admin
 import "net/http"
 
 func home(w http.ResponseWriter, r *http.Request) {
-	type props struct {
-		Title string
-	}
-
-	renderTemplate(w, "admin/html/home.html", props{
-        Title: "home",
-	})
+	renderTemplate(w, "admin/html/home.html", nil)
 }
 
--- admin/writing.go
+++ admin/writing.go
@@ -3,11 +3,5 @@ package admin
 import "net/http"
 
 func writing(w http.ResponseWriter, r *http.Request) {
-	type props struct {
-		Title string
-	}
-
-	renderTemplate(w, "admin/html/writing.html", props{
-		Title: "writing",
-	})
+	renderTemplate(w, "admin/html/writing.html", nil)
 }
